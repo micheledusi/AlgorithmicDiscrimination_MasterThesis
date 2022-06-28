895c6479a028cb757cee742efbc46c3cb5ad7df6
MicheleDusi <micheledusi.618@gmail.com>
2022-06-28 17:39:36 +0200

    Refactor the code by separating classifier code

--- src/experiments/gender_subspace_detection.py
+++ src/experiments/gender_subspace_detection.py
@@ -10,24 +10,29 @@
 
 import numpy as np
 import torch
-from sklearn import svm
 
 import settings
 from src.models.gender_enum import Gender
+from src.models.gender_subspace_model import GenderSubspaceModel
 from src.models.word_encoder import WordEncoder
 from src.parsers.winogender_occupations_parser import OccupationsParser
+from src.parsers.jneidel_occupations_parser import ONEWORD_OCCUPATIONS
+
 
 gendered_words: dict[Gender, list[str]] = {
-	Gender.MALE: ["he", "him", "his", "male", "boy", "man", "father", "dad", "daddy", "sir", "king"],
-	Gender.FEMALE: ["she", "her", "female", "girl", "woman", "mother", "mom", "mommy", "madam", "queen"],
+	Gender.MALE: ["he", "him", "his", "male", "boy", "man", "father", "dad", "daddy", "sir", "king", "masculinity",
+	              "lord", "uncle", "grandpa", "grandfather", "husband"],
+	Gender.FEMALE: ["she", "her", "female", "girl", "woman", "mother", "mom", "mommy", "madam", "queen", "femininity",
+	                "lady", "aunt", "grandma", "grandmother", "wife"],
 }
 
-LAYER: int = 12
+LAYERS: range = range(13)
 
 
 def detect_gender_direction(encoder: WordEncoder):
 
-	target_words: list[str] = OccupationsParser().occupations_list
+	# target_words: list[str] = OccupationsParser().occupations_list
+	target_words: list[str] = ONEWORD_OCCUPATIONS
 
 	train_x: list[np.ndarray] = []
 	train_y: list[Gender] = []
@@ -36,39 +41,39 @@ def detect_gender_direction(encoder: WordEncoder):
 	with torch.no_grad():
 		for gend, words in gendered_words.items():
 			for w in words:
-				embedding = encoder.embed_word_merged(w, layers=[LAYER]).detach().numpy()[0]
+				embedding = encoder.embed_word_merged(w, layers=LAYERS).detach().numpy()
 				train_x.append(embedding)
 				train_y.append(gend)
 		for tw in target_words:
-			embedding = encoder.embed_word_merged(tw, layers=[LAYER]).detach().numpy()[0]
+			embedding = encoder.embed_word_merged(tw, layers=LAYERS).detach().numpy()
 			eval_x.append(embedding)
 
 	print("Training Dataset   - length: ", len(train_x))
 	print("Evaluation Dataset - length: ", len(eval_x))
 
-	print("Training Support Vector Classifier...", end='')
-	classifier = svm.LinearSVC()
-	classifier.fit(train_x, train_y)
-	print("Completed.")
+	# Training the gender subspace division model
+	subspace_model = GenderSubspaceModel(embeddings=np.asarray(train_x), genders=train_y)
 
-	print("Predicting target words...", end='')
-	predicted_y = classifier.predict(eval_x)
-	coefficients = classifier.coef_[0]
-	print("Completed.")
+	predicted_gender = subspace_model.predict(embeddings=np.asarray(eval_x))
+	projected_gender = subspace_model.project(embeddings=np.asarray(eval_x))
 
-	print("Results:")
 	with open(f"{settings.FOLDER_RESULTS}/gender_subspace_detection/tables/occupations_static_spectrum.tsv", "w") as f:
-		print(settings.OUTPUT_TABLE_COL_SEPARATOR.join(["word", "gender", "gender_name", "gender_projection"]), file=f)
-		for x, y, tw in zip(eval_x, predicted_y, target_words):
-			spectrum = np.dot(x, coefficients)
-			print(f"{tw:20s}: {Gender(y).name:6s} : {spectrum}")
-			print(tw, end=settings.OUTPUT_TABLE_COL_SEPARATOR, file=f)
-			print(Gender(y), end=settings.OUTPUT_TABLE_COL_SEPARATOR, file=f)
-			print(Gender(y).name, end=settings.OUTPUT_TABLE_COL_SEPARATOR, file=f)
-			print(spectrum, file=f)
+		# Printing the header
+		header_list: list[str] = ["word"]
+		header_list.extend([f"pred_gender_{layer:02d}" for layer in range(subspace_model.num_layers)])
+		header_list.extend([f"proj_gender_{layer:02d}" for layer in range(subspace_model.num_layers)])
+		print(settings.OUTPUT_TABLE_COL_SEPARATOR.join(header_list), file=f)
+
+		# Printing the table
+		for word, pred, proj in zip(target_words, predicted_gender, projected_gender):
+			row_list = [word]
+			row_list.extend(map(str, pred))
+			row_list.extend(map(str, proj))
+			print(settings.OUTPUT_TABLE_COL_SEPARATOR.join(row_list), file=f)
 
 
 def launch() -> None:
 	enc = WordEncoder()
+	# Detecting the gender direction
 	detect_gender_direction(enc)
 	return
---
+++ src/models/gender_subspace_model.py
@@ -0,0 +1,58 @@
+#########################################################################
+#                            Dusi's Thesis                              #
+# Algorithmic Discrimination and Natural Language Processing Techniques #
+#########################################################################
+
+# This model tries to define a gender subspace in the embeddings space, for each layers.
+# For the sake of simplicity and analysis, we'll consider only male and female genders, which
+# is not a socially correct assumption.
+
+from enum import IntEnum
+
+import numpy as np
+from sklearn import svm
+
+from src.models.gender_enum import Gender
+
+
+class GenderSubspaceModel:
+	__classifiers: list[svm.LinearSVC] = []
+	__num_features: int
+
+	def __init__(self, embeddings: np.ndarray, genders: list[IntEnum] | list[int]) -> None:
+		"""
+		Creates and train the hyperplane that separates genders.
+		:param embeddings: A 3D matrix of embeddings: [# samples, # layers = 13, # features = 768]
+		:param genders: A list of [# samples] gender, for the embeddings
+		"""
+		num_samples, num_layers, self.__num_features = embeddings.shape
+		assert num_samples == len(genders)
+		print("Training samples: ", num_samples)
+		print("Number of layers: ", num_layers)
+		print("Number of features: ", self.__num_features)
+
+		# Training a classifier for each layer
+		for layer in range(num_layers):
+			clf = svm.LinearSVC()
+			clf.fit(embeddings[:, layer], genders)
+			self.__classifiers.append(clf)
+
+	@property
+	def num_layers(self) -> int:
+		return len(self.__classifiers)
+
+	def predict(self, embeddings: np.ndarray) -> np.ndarray:
+		predictions = np.zeros(shape=(len(embeddings), self.num_layers), dtype=np.uint8)
+		for layer, clf in enumerate(self.__classifiers):
+			predictions[:, layer] = clf.predict(embeddings[:, layer])
+		return predictions
+
+	def project(self, embeddings: np.ndarray) -> np.ndarray:
+		projections = np.zeros(shape=(len(embeddings), self.num_layers), dtype=np.float)
+		for layer, clf in enumerate(self.__classifiers):
+			coefs = clf.coef_[0]
+			projections[:, layer] = np.dot(embeddings[:, layer], coefs)
+		return projections
+
+
+
