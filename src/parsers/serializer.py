b25571a6cb62cc22b27f036873215c89ef901b11
MicheleDusi <micheledusi.618@gmail.com>
2022-07-19 17:46:19 +0200

    Add gendered_words embeddings to the serialized available data

--- src/parsers/serializer.py
+++ src/parsers/serializer.py
@@ -4,18 +4,23 @@
 #########################################################################
 
 # Serializes data by loading and saving binary files
-
+import os
 import pickle
 from typing import Any
 
 import numpy as np
 import torch
+from datasets import Dataset, DatasetDict
 
 import settings
+from src.experiments.embeddings_gender_subspace_detection import gendered_words
+from src.models.gender_enum import Gender
 from src.models.word_encoder import WordEncoder
 from src.parsers import jobs_parser
 
 DEFAULT_ROOT_PATH = settings.FOLDER_SAVED_DATA
+SUBDIR_EMBEDDINGS = 'embeddings/'
+SUBDIR_DATASETS = 'datasets/'
 
 
 class Serializer:
@@ -42,10 +47,13 @@ class Serializer:
 			return pickle.load(file=f)
 
 	def save_embeddings(self, embeddings: np.ndarray | torch.Tensor, file_id: str) -> None:
-		self.save(obj=embeddings, file_id='embeddings/' + file_id)
+		self.save(obj=embeddings, file_id=SUBDIR_EMBEDDINGS + file_id)
+
+	def save_dataset(self, dataset: Dataset | DatasetDict, file_id: str) -> None:
+		self.save(obj=dataset, file_id=SUBDIR_DATASETS + file_id)
 
 	def load_embeddings(self, file_id: str, array_type: str = 'np') -> np.ndarray | torch.Tensor:
-		embeddings = self.load(file_id='embeddings/' + file_id)
+		embeddings = self.load(file_id=SUBDIR_EMBEDDINGS + file_id)
 		match array_type:
 			case 'np':
 				if isinstance(embeddings, np.ndarray):
@@ -61,24 +69,61 @@ class Serializer:
 				raise AttributeError("Unknown requested type for embeddings array: " + array_type)
 		raise ImportError("Cannot load embeddings as the requested type")
 
+	def load_dataset(self, file_id: str) -> Dataset | DatasetDict:
+		dataset = self.load(file_id=SUBDIR_DATASETS + file_id)
+		assert isinstance(dataset, Dataset | DatasetDict)
+		return dataset
+
+
+FORCE_REWRITE: bool = False
 
 if __name__ == "__main__":
-	jobs: list[str] = jobs_parser.get_words_list(jobs_parser.DEFAULT_FILEPATH)
-	print("Encoding default jobs list...")
-	encoder: WordEncoder = WordEncoder()
-	embeddings_list: list[torch.Tensor] = []
-	with torch.no_grad():
-		for word in jobs:
-			embeddings_list.append(
-				encoder.embed_word_merged(word).cpu()
-			)
-	embeddings_arr: torch.Tensor = torch.stack(embeddings_list)
-	print("Dumping embeddings...")
-	serializer = Serializer()
-	serializer.save_embeddings(embeddings_arr, 'jobs')
-	print("Completed.")
 
-	print("Re-loading values...")
-	embs = serializer.load_embeddings('jobs', 'np')
-	print("Shape of numpy array: ", embs.shape)
+	if FORCE_REWRITE or not os.path.isfile(DEFAULT_ROOT_PATH + '/' + SUBDIR_EMBEDDINGS + 'jobs.pkl'):
+		jobs: list[str] = jobs_parser.get_words_list(jobs_parser.DEFAULT_FILEPATH)
+		print("Encoding default jobs list...")
+		encoder: WordEncoder = WordEncoder()
+		embeddings_list: list[torch.Tensor] = []
+		with torch.no_grad():
+			for word in jobs:
+				embeddings_list.append(
+					encoder.embed_word_merged(word).cpu()
+				)
+		embeddings_arr: torch.Tensor = torch.stack(embeddings_list)
+		print("Dumping embeddings...")
+		serializer = Serializer()
+		serializer.save_embeddings(embeddings_arr, 'jobs')
+		print("Completed.")
+
+		print("Re-loading values...")
+		embs = serializer.load_embeddings('jobs', 'np')
+		print("Shape of numpy array: ", embs.shape)
+
+	if FORCE_REWRITE or not os.path.isfile(DEFAULT_ROOT_PATH + '/' + SUBDIR_DATASETS + 'gendered_words.pkl'):
+		encoder: WordEncoder = WordEncoder()
+		words_list: list[str] = []
+		genders_list: list[Gender] = []
+		embeddings_list: list[torch.Tensor] = []
+		# Building lists
+		for g, words in gendered_words.items():
+			for w in words:
+				words_list.append(w)
+				genders_list.append(g)
+				embeddings_list.append(encoder.embed_word_merged(w))
+		# Aggregating lists into a dataset
+		gendered_dataset = Dataset.from_dict({'word': words_list, 'gender': genders_list, 'embedding': embeddings_list})
+
+		# Serializing dataset
+		print("Dumping dataset...", end="")
+		serializer = Serializer()
+		serializer.save_dataset(gendered_dataset, 'gendered_words')
+		print("Completed.")
+
+		print("Re-loading values...")
+		ds = serializer.load_dataset('gendered_words')
+		print("Length of dataset: ", len(ds))
+		print("Features of dataset: ", ds.features)
+
+	pass
+
 
